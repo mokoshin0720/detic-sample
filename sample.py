15525feb689fc89150f7319c1cf58313f668e287
mokoshin0720 <shinya.work0720@gmail.com>
2022-10-30 20:58:18 +0900

    [add]confirm run in local pc

--- sample.py
+++ sample.py
@@ -1,29 +1,23 @@
 from detectron2.utils.logger import setup_logger
 setup_logger()
 
-# import sys
-# import numpy as np
-# import os, json, cv2, random
+import sys
+import numpy as np
+import os, json, cv2, random
 # from google.colab.patches import cv2_imshow
 
-# from detectron2 import model_zoo
-# from detectron2.engine import DefaultPredictor
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-# from detectron2.utils.visualizer import Visualizer
-# from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
 
-
-# sys.path.insert(0, 'third_party/CenterNet2/projects/CenterNet2/')
-# from centernet.config import add_centernet_config
-# from detic.config import add_detic_config
-# from detic.modeling.utils import reset_cls_test
 
 cfg = get_cfg()
-# add_centernet_config(cfg)
-# add_detic_config(cfg)
-# cfg.merge_from_file("configs/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.yaml")
-# cfg.MODEL.WEIGHTS = 'https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth'
-# cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-# cfg.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'rand'
-# cfg.MODEL.ROI_HEADS.ONE_CLASS_PER_PROPOSAL = True # For better visualization purpose. Set to False for all classes.
-# predictor = DefaultPredictor(cfg)
+# add project-specific config (e.g., TensorMask) here if you're not running a model in detectron2's core library
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+# Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+predictor = DefaultPredictor(cfg)
+outputs = predictor(im)
--- src/detectron2/model_zoo/model_zoo.py
+++ src/detectron2/model_zoo/model_zoo.py
@@ -136,9 +136,12 @@ def get_config_file(config_path):
     Returns:
         str: the real path to the config file.
     """
-    cfg_file = pkg_resources.resource_filename(
-        "detectron2.model_zoo", os.path.join("configs", config_path)
-    )
+    # cfg_file = pkg_resources.resource_filename(
+    #     "detectron2.model_zoo", os.path.join("configs", config_path)
+    # )
+
+    cfg_file = os.path.join('src/configs', config_path)
+
     if not os.path.exists(cfg_file):
         raise RuntimeError("{} not available in Model Zoo!".format(config_path))
     return cfg_file
