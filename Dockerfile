7bac4c48532a9509d6f0683ccd84339e6b3641f8
mokoshin0720 <shinya.work0720@gmail.com>
2022-11-03 16:10:19 +0900

    [add]prepare gpu env

--- Dockerfile
+++ Dockerfile
@@ -12,3 +12,4 @@ RUN apt-get -y install libgl1-mesa-dev
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+RUN pip3 install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
--- sample.py
+++ sample.py
@@ -20,4 +20,8 @@ cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 predictor = DefaultPredictor(cfg)
+im = cv2.imread("./data/resized/zinaida-serebriakova_woman-in-blue-1934.jpg")
 outputs = predictor(im)
+v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+cv2.imwrite('test.png', out.get_image()[:, :, ::-1])
